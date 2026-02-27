f43283db6de760c18fce25e2075ec170d479915a
Dasha <dasha26027@icloud.com>
2026-02-27 17:28:25 +0400

    fix import

--- less2/main.go
+++ less2/main.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"fmt"
+
+	"gitlab.com/DaryaButuzova/practice2026/less2/stack"
+)
+
 func main() {
-	st := NewStack(5)
-	Push(st, 10)
-	fmt.println(Pop(st))
+
+	s := stack.NewStack(5)
+
+	stack.Push(s, 10)
+	stack.Push(s, 20)
+	stack.Push(s, 30)
+
+	fmt.Println("Верхний элемент:", stack.Peek(s)) // Должно быть 30
+
+	fmt.Println("Извлекли:", stack.Pop(s)) // 30
+	fmt.Println("Извлекли:", stack.Pop(s)) // 20
+
+	fmt.Println("Верхний элемент после извлечения:", stack.Peek(s)) // 10
 }
